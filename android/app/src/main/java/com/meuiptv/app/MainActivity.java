bff352ecabf892eb1b1368c0313f0cacaca62ec7
satoro012 <solitariogojo@gmail.com>
2026-09-14 22:13:32 -0300

    registra plugin VideoPlayer no MainActivity

--- android/app/src/main/java/com/meuiptv/app/MainActivity.java
+++ android/app/src/main/java/com/meuiptv/app/MainActivity.java
@@ -1,5 +1,13 @@
 package com.meuiptv.app;
 
+import android.os.Bundle;
 import com.getcapacitor.BridgeActivity;
+import com.meuiptv.plugin.VideoPlayerPlugin;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        registerPlugin(VideoPlayerPlugin.class);
+        super.onCreate(savedInstanceState);
+    }
+}
