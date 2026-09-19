10b78ac9e4c76bd603c85261ee403aa63a68c5fc
satoro012 <solitariogojo@gmail.com>
2026-09-19 16:07:18 -0300

    atualiza url do painel

--- www/js/config.js
+++ www/js/config.js
@@ -6,4 +6,4 @@
  */
 const TENANT_ID = "tenant_TESTE"; // <- troca pelo ID gerado no painel
 const APP_SECRET = "um_segredo_bem_forte_aqui_troque_isso_123xyz"; // <- mesmo valor do netlify env:set APP_SECRET
-const PAINEL_BASE_URL = "https://SUA_URL_NETLIFY.netlify.app"; // <- troca depois do deploy
+const PAINEL_BASE_URL = "https://hfweed.netlify.app"; // <- troca depois do deploy
